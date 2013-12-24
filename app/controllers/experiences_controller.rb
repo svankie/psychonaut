4f0d0f1c0ad63e288c27b8d9b9d09afc5b88dea2
Horacio Bertorello <svankie@gmail.com>
2013-12-24 01:43:49 -0300

    I have no idea.

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,6 +1,14 @@
 # encoding: utf-8
 
 class ExperiencesController < ApplicationController
+  def index
+    @experiences = if params[:query]
+                    Experience.search(params[:query])
+                  else
+                    Experience.paginate(page: params[:page], per_page: 10).order('happened_at desc')
+                  end
+  end
+  
   def new
     @experience = Experience.new
   end
